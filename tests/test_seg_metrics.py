cf4586a3ceeba2d1b16544bcaa192129de861e10
m_helleis <Max.Helleis@dlr.de>
2021-06-01 15:39:07 +0200

    blacked

--- tests/test_seg_metrics.py
+++ tests/test_seg_metrics.py
@@ -11,32 +11,28 @@ def test_tpfptnfn():
     y_pred = np.ones(shape)
     valid_mask = np.ones(shape)
     tpfptnfn = segm.tpfptnfn(y_true, y_pred, valid_mask)
-    assert (tpfptnfn["tp"] == y_true.size) and (tpfptnfn["fp"] == 0) and (tpfptnfn["tn"] == 0 and (tpfptnfn[
-        "fn"] == 0))
+    assert (tpfptnfn["tp"] == y_true.size) and (tpfptnfn["fp"] == 0) and (tpfptnfn["tn"] == 0 and (tpfptnfn["fn"] == 0))
 
     # case no pixel belongs to the positive class and none are detected (which is correct)
     y_true = np.zeros(shape)
     y_pred = np.zeros(shape)
     valid_mask = np.ones(shape)
     tpfptnfn = segm.tpfptnfn(y_true, y_pred, valid_mask)
-    assert (tpfptnfn["tn"] == y_true.size) and (tpfptnfn["fp"] == 0) and (tpfptnfn["tp"] == 0 and (tpfptnfn[
-                                                                                                       "fn"] == 0))
+    assert (tpfptnfn["tn"] == y_true.size) and (tpfptnfn["fp"] == 0) and (tpfptnfn["tp"] == 0 and (tpfptnfn["fn"] == 0))
 
     # case all pixels belong to the positive class but none are detected
     y_true = np.ones(shape)
     y_pred = np.zeros(shape)
     valid_mask = np.ones(shape)
     tpfptnfn = segm.tpfptnfn(y_true, y_pred, valid_mask)
-    assert (tpfptnfn["fn"] == y_true.size) and (tpfptnfn["fp"] == 0) and (tpfptnfn["tn"] == 0 and (tpfptnfn[
-                                                                                                       "tp"] == 0))
+    assert (tpfptnfn["fn"] == y_true.size) and (tpfptnfn["fp"] == 0) and (tpfptnfn["tn"] == 0 and (tpfptnfn["tp"] == 0))
 
     # case no pixels belong to the positive class but all are wrongly detected
     y_true = np.zeros(shape)
     y_pred = np.ones(shape)
     valid_mask = np.ones(shape)
     tpfptnfn = segm.tpfptnfn(y_true, y_pred, valid_mask)
-    assert (tpfptnfn["fp"] == y_true.size) and (tpfptnfn["fn"] == 0) and (tpfptnfn["tn"] == 0 and (tpfptnfn[
-                                                                                                       "tp"] == 0))
+    assert (tpfptnfn["fp"] == y_true.size) and (tpfptnfn["fn"] == 0) and (tpfptnfn["tn"] == 0 and (tpfptnfn["tp"] == 0))
 
     # if x pixels are marked as non valid in the valid mask, the total sum of tpfptnfn must shrink by x
     y_true = np.zeros(shape)
@@ -48,8 +44,12 @@ def test_tpfptnfn():
     valid_mask[:x, :y, :z] = 0
     tpfptnfn = segm.tpfptnfn(y_true, y_pred, valid_mask)
 
-    assert np.sum(valid_mask) == np.sum([tpfptnfn["tp"], tpfptnfn["fp"], tpfptnfn["tn"], tpfptnfn["fn"]]) == tpfptnfn[
-        "n_valid_pixel"]
+    assert (
+        np.sum(valid_mask)
+        == np.sum([tpfptnfn["tp"], tpfptnfn["fp"], tpfptnfn["tn"], tpfptnfn["fn"]])
+        == tpfptnfn["n_valid_pixel"]
+    )
+
 
 def test_accuracy():
     # define reference mask (y_true), predictions (y_pred) and a valid pixel mask
@@ -140,6 +140,10 @@ def test_kappa():
     y_true_sk = y_true[valid_mask == 1]
     y_pred_sk = y_pred[valid_mask == 1]
     tpfptnfn = segm.tpfptnfn(y_true, y_pred, valid_mask)
-    assert np.round(segm._kappa(
-        tpfptnfn["tp"], tpfptnfn["fp"], tpfptnfn["tn"], tpfptnfn["fn"], tpfptnfn["n_valid_pixel"]), decimals=6) == \
-           np.round(cohen_kappa_score(y_true_sk.flatten(), y_pred_sk.flatten()), decimals=6)
+    assert (
+        np.round(
+            segm._kappa(tpfptnfn["tp"], tpfptnfn["fp"], tpfptnfn["tn"], tpfptnfn["fn"], tpfptnfn["n_valid_pixel"]),
+            decimals=6,
+        )
+        == np.round(cohen_kappa_score(y_true_sk.flatten(), y_pred_sk.flatten()), decimals=6)
+    )
