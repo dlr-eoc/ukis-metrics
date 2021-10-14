8342ec316b954a63638ed513d1cf2523627ec2e1
Fichtner, Florian Willy <florian.fichtner@dlr.de>
2021-10-14 16:31:44 +0200

    pathlib

--- tests/test_ukis_metrics.py
+++ tests/test_ukis_metrics.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 import toml
 from ukis_metrics import __version__
 
 
 def test_version():
-    with open("../pyproject.toml") as f:
+    with open(Path.cwd().joinpath("pyproject.toml")) as f:
         assert __version__ == toml.load(f).get("tool").get("poetry").get("version")
